0ffc21525e725294ea9f554b3c1e484558c5ed77
Antti Helander <hela_ant@outlook.com>
2021-12-18 20:47:33 +0200

    Play returns sql with multiple values from one column. Continuing to make class that return multiple values from sql query.

--- app/controllers/muumimukit.scala
+++ app/controllers/muumimukit.scala
@@ -8,45 +8,50 @@ import java.sql.ResultSet
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-case class Product(
+/*case class Product(
                     nimi: String,
-                    price: Int)
+                    price: Int)*/
 
 
 class muumimukit  @Inject() (val controllerComponents: ControllerComponents,db: Database, databaseExecutionContext: ExecutionContext) extends BaseController {
 
   implicit val ec: ExecutionContext = databaseExecutionContext
 
-  def worksnot() = Action { implicit request: Request[AnyContent] =>
+  def notworks = Action { implicit request: Request[AnyContent] =>
 
-    val mukit = List("Joo", "Testi")
-    val hah = List("hah", "hah2")
+    val mukit = List("Joo", "Testi", "pöö")
+    val hah = List("hah", "hah2", "pöö")
 
-    Ok(views.html.muumimukit(mukit))
+    Ok(views.html.muumimukit(mukit, hah))
+
   }
 
-  def works: Action[AnyContent] = Action async { implicit request: Request[AnyContent] =>
+  /*def works: Action[AnyContent] = Action async { implicit request: Request[AnyContent] =>
     for {
       resultDb <- doSomething()
+
     } yield {
-      Ok(views.html.muumimukit(resultDb))
+
+     Ok(views.html.muumimukit(resultDb))
+      val resultJsonObj = Json.toJson(resultDb)
+      Ok(resultJsonObj)
     }
 
   }
 
 
-  def doSomething()(implicit databaseExecutionContext: ExecutionContext): Future[List[Int]] = {
+  def doSomething()(implicit databaseExecutionContext: ExecutionContext): Future[List[(String, Int)]] = {
     Future {
       db.withConnection { conn =>
-        val resultSet:ResultSet = conn.prepareStatement("select, userid, nimi, hinta from muumimukit").executeQuery()
-        new Iterator[Int] {
-          Product(nimi = resultSet.getString("nimi"), price = resultSet.getInt("hinta"))
-          def hasNext = resultSet.next()
-          def next() = resultSet.getInt("userid")
+        val resultSet:ResultSet = conn.prepareStatement("select nimi, hinta from muumimukit").executeQuery()
+        new Iterator[(String, Int)] {
+          def hasNext: Boolean = resultSet.next()
+          def next(): (Int, String) = (resultSet.getInt("numero"),resultSet.getString("nimi"))
         }.toList
       }
     }(databaseExecutionContext)
-  }
+  }*/
+
 
 
 
